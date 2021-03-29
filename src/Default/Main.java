d49cdc388b3552a4e3596c8aeb3de887803d6103
Pedrcc17 <pmbcc@iscte-iul.pt>
2021-03-29 22:43:05 +0100

    SmellyClass

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -5,15 +5,17 @@ import java.io.FileReader;
 
 public class Main {
 
-	public Main(String args) {
-
-	}
-
 	public static void main(String[] args) {
 		try {
 			BufferedReader javaFile = new BufferedReader(new FileReader("src\\Default\\RandomizedQueue.java"));
-			if(javaFile.readLine().split(" ")[0] != "import") {
-				System.out.println("Hooray");
+			String[] word = javaFile.readLine().split(" ");
+			System.out.println(word[1]);
+			while(word[1].equals("package") ||word[1].equals("import") || word[1].equals("")) {
+				System.out.println("Import or package, ignore.");
+				word[1] = javaFile.readLine().split(" ")[0];
+			}
+			if(word[2] == "class") {
+				SmellyClass classy = new SmellyClass(javaFile);
 			}
 		} catch (Exception e) {
 			System.out.println("Erro a abrir o ficheiro");
---
+++ src/Default/SmellyClass.java
@@ -0,0 +1,13 @@
+package Default;
+
+import java.io.BufferedReader;
+
+public class SmellyClass {
+	private BufferedReader javaFile;
+
+	public SmellyClass(BufferedReader in) {
+		javaFile = in;
+
+	}
+
+}
