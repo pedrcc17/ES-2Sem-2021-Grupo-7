6c7fd934c0f07555510b7d9573fac8ae5cec35d0
Pedrcc17 <pmbcc@iscte-iul.pt>
2021-03-31 18:46:38 +0100

    yu

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -5,23 +5,28 @@ import java.io.FileReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try {
-			BufferedReader javaFile = new BufferedReader(new FileReader("src\\Default\\RandomizedQueue.java"));
-			String[] word = javaFile.readLine().split(" ");
-			System.out.println(word[0]);
-			while (word[0].equals("package") || word[0].equals("import") || word[0].equals("")) {
-				System.out.println("Import or package, ignore.");
-				word = javaFile.readLine().split(" ");
-			}
-			System.out.println(word[1]);
-			if (word[1].equals("class")) {
-				SmellyClass classy = new SmellyClass();
-				classy.NOM(javaFile);
-				classy.WMC(javaFile);
-			}
-		} catch (Exception e) {
-			System.out.println("Erro a abrir o ficheiro");
-		}
-	}
+    private static BufferedReader restartReading() {
+        try {
+            BufferedReader javaFile = new BufferedReader(new FileReader("src\\Default\\RandomizedQueue.java"));
+            String[] word = javaFile.readLine().split(" ");
+            System.out.println(word[0]);
+            while (word[0].equals("package") || word[0].equals("import") || word[0].equals("")) {
+                System.out.println("Import or package, ignore.");
+                word = javaFile.readLine().split(" ");
+            }
+            System.out.println(word[1]);
+            if (word[1].equals("class")) {
+                return javaFile;
+            }
+        } catch (Exception e) {
+            System.out.println("Erro a abrir o ficheiro");
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        SmellyClass classy = new SmellyClass();
+        classy.NOM(restartReading());
+        classy.WMC(restartReading());
+    }
 }
