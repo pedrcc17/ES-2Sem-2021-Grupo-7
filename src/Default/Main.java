d8166a1c16bb6a3ded489b2c5f387d4c48a50023
sabrab <luismbarbas@gmail.com>
2021-04-02 00:06:46 +0100

    filechooser

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -1,13 +1,17 @@
 package Default;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FilenameFilter;
+
+import javax.swing.JFileChooser;
 
 public class Main {
 
-    private static BufferedReader restartReading() {
+    private static BufferedReader restartReading(String file) {
         try {
-            BufferedReader javaFile = new BufferedReader(new FileReader("src\\Default\\RandomizedQueue.java"));
+            BufferedReader javaFile = new BufferedReader(new FileReader(file));
             String[] word = javaFile.readLine().split(" ");
             while (word[0].equals("package") || word[0].equals("import") || word[0].equals("")) {
                 word = javaFile.readLine().split(" ");
@@ -15,26 +19,41 @@ public class Main {
             if (word[1].equals("class")) {
                 return javaFile;
             }
-        } catch (Exception e) {
+        			}
+        			catch (Exception e) {
             System.out.println("Erro a abrir o ficheiro");
         }
         return null;
     }
-
+    
     public static void main(String[] args) {
-        SmellyClass classy = new SmellyClass();
-        classy.NOM(restartReading());
-        System.out.println("-------------------------");
-        classy.WMC(restartReading());
-        System.out.println("-------------------------");
-        classy.CYCLO_Method(restartReading());
-        System.out.println("-------------------------");
-        classy.LOC_Method(restartReading());
-        System.out.println("-------------------------");
-        classy.LOC_Class(restartReading());
-        System.out.println("-------------------------");
-        classy.isLongMethod(5, 2, true, true, false);
-        System.out.println("-------------------------");
-        classy.isGodClass(15, 10, 100, true, true, true, true, true);
+    	File fileDirectory = null;
+    	String[] fileDirectorylist = null;
+    	JFileChooser chooser = new JFileChooser(".");
+    	chooser.setMultiSelectionEnabled(true);
+    	chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+    	int ret = chooser.showOpenDialog(null);
+    	if(ret == JFileChooser.APPROVE_OPTION) {
+    	    fileDirectory = chooser.getSelectedFile();
+    	    fileDirectorylist = fileDirectory.list();
+    	    System.out.println(fileDirectory);
+			for (String file : fileDirectorylist) {
+				System.out.println(file);
+    	        SmellyClass classy = new SmellyClass();
+    	        classy.NOM(restartReading(file));
+    	        System.out.println("-------------------------");
+    	        classy.WMC(restartReading(file));
+    	        System.out.println("-------------------------");
+    	        classy.CYCLO_Method(restartReading(file));
+    	        System.out.println("-------------------------");
+    	        classy.LOC_Method(restartReading(file));
+    	        System.out.println("-------------------------");
+    	        classy.LOC_Class(restartReading(file));
+    	        System.out.println("-------------------------");
+    	        classy.isLongMethod(5, 2, true, true, false);
+    	        System.out.println("-------------------------");
+    	        classy.isGodClass(15, 10, 100, true, true, true, true, true);
+    	    	}
+    	}
     }
 }
