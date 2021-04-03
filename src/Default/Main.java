de44b715706ba4f20e337a383850d0281235e7bf
afrva-iscte <afrva@iscte-iul.pt>
2021-04-03 15:39:00 +0100

    excel name choosing

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -6,57 +6,68 @@ import java.io.FileReader;
 import java.io.FilenameFilter;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class Main {
 
-    private static BufferedReader restartReading(String dir, String file) {
-        try {
-            BufferedReader javaFile = new BufferedReader(new FileReader(dir + "\\" + file));
-            String[] word = javaFile.readLine().split(" ");
-            while (word[0].equals("package") || word[0].equals("import") || word[0].equals("")) {
-                word = javaFile.readLine().split(" ");
-            }
-            if (word[1].equals("class")) {
-                return javaFile;
-            }
-        			}
-        			catch (Exception e) {
-            System.out.println("Erro a abrir o ficheiro");
-        }
-        return null;
-    }
-    
-    public static void main(String[] args) {
-    	File fileDirectory = null;
-    	File filePath = null;
-    	String[] fileDirectorylist = null;
-    	String filePathString = null;
-    	JFileChooser chooser = new JFileChooser(".");
-    	chooser.setMultiSelectionEnabled(true);
-    	chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-    	int ret = chooser.showOpenDialog(null);
-    	if(ret == JFileChooser.APPROVE_OPTION) {
-    	    fileDirectory = chooser.getSelectedFile();
-    	    fileDirectorylist = fileDirectory.list();
-    	    filePathString = fileDirectory.getPath();
-    	    System.out.println(fileDirectory);
+	private static BufferedReader restartReading(String dir, String file) {
+		try {
+			BufferedReader javaFile = new BufferedReader(new FileReader(dir + "\\" + file));
+			String[] word = javaFile.readLine().split(" ");
+			while (word[0].equals("package") || word[0].equals("import") || word[0].equals("")) {
+				word = javaFile.readLine().split(" ");
+			}
+			if (word[1].equals("class")) {
+				return javaFile;
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Erro a abrir o ficheiro");
+		}
+		return null;
+	}
+
+	private static String fileName( ) {
+		String s = null;
+		while (s == null) {
+			s = (String)JOptionPane.showInputDialog(null, "Choose Excel file name", JOptionPane.PLAIN_MESSAGE);
+		}
+		return s;
+	}
+
+	public static void main(String[] args) {
+		String name = fileName();
+		System.out.println(name);
+		File fileDirectory = null;
+		File filePath = null;
+		String[] fileDirectorylist = null;
+		String filePathString = null;
+		JFileChooser chooser = new JFileChooser(".");
+		chooser.setMultiSelectionEnabled(true);
+		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int ret = chooser.showOpenDialog(null);
+		if(ret == JFileChooser.APPROVE_OPTION) {
+			fileDirectory = chooser.getSelectedFile();
+			fileDirectorylist = fileDirectory.list();
+			filePathString = fileDirectory.getPath();
+			System.out.println(fileDirectory);
 			for (String file : fileDirectorylist) {
 				System.out.println(file);
-    	        SmellyClass classy = new SmellyClass();
-    	        classy.NOM(restartReading(filePathString, file));
-    	        System.out.println("-------------------------");
-    	        classy.WMC(restartReading(filePathString, file));
-    	        System.out.println("-------------------------");
-    	        classy.CYCLO_Method(restartReading(filePathString, file));
-    	        System.out.println("-------------------------");
-    	        classy.LOC_Method(restartReading(filePathString, file));
-    	        System.out.println("-------------------------");
-    	        classy.LOC_Class(restartReading(filePathString, file));
-    	        System.out.println("-------------------------");
-    	        classy.isLongMethod(5, 2, true, true, false);
-    	        System.out.println("-------------------------");
-    	        classy.isGodClass(15, 10, 100, true, true, true, true, true);
-    	    	}
-    	}
-    }
+				SmellyClass classy = new SmellyClass();
+				classy.NOM(restartReading(filePathString, file));
+				System.out.println("-------------------------");
+				classy.WMC(restartReading(filePathString, file));
+				System.out.println("-------------------------");
+				classy.CYCLO_Method(restartReading(filePathString, file));
+				System.out.println("-------------------------");
+				classy.LOC_Method(restartReading(filePathString, file));
+				System.out.println("-------------------------");
+				classy.LOC_Class(restartReading(filePathString, file));
+				System.out.println("-------------------------");
+				classy.isLongMethod(5, 2, true, true, false);
+				System.out.println("-------------------------");
+				classy.isGodClass(15, 10, 100, true, true, true, true, true);
+			}
+		}
+	}
 }
