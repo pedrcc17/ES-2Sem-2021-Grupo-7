ea8a860659460e971312f49c45e777ea24dd3598
afrva-iscte <afrva@iscte-iul.pt>
2021-04-20 17:17:20 +0100

    Added Main's "main" some lines to test Excel Importing

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 
 import javax.swing.JFileChooser;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
 public class Main {
 
 	private static BufferedReader restartReading(String dir, String file) {
@@ -25,74 +27,76 @@ public class Main {
 			System.out.println("Erro a abrir o ficheiro");
 		}
 		return null;
-		
+
 		//outroutot
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InvalidFormatException {
 		ExcelExporting eexport = new ExcelExporting();
 		ArrayList<File> allJavaFiles = readEverything();
-			for (File file : allJavaFiles) {
-				System.out.println("files " + file);
-				SmellyClass classy = new SmellyClass();
-				classy.NOM(restartReading(file.getParent(), file.getName()));
-				System.out.println("-------------------------");
-				classy.WMC(restartReading(file.getParent(), file.getName()));
-				System.out.println("-------------------------");
-				classy.CYCLO_Method(restartReading(file.getParent(), file.getName()));
-				System.out.println("-------------------------");
-				classy.LOC_Method(restartReading(file.getParent(), file.getName()));
-				System.out.println("-------------------------");
-				classy.LOC_Class(restartReading(file.getParent(), file.getName()));
-				System.out.println("-------------------------");
-				classy.isLongMethod(5, 2, true, true, false);
-				System.out.println("-------------------------");
-				classy.isGodClass(15, 10, 100, true, true, true, true, true);
-				eexport.setClassAndPackageNames(file.getName(), file.getParent());
-				eexport.saveMetrics(classy);
-			}
-			eexport.exportToExcel(eexport.chooseName());
-			
-			//teste
+		for (File file : allJavaFiles) {
+			System.out.println("files " + file);
+			SmellyClass classy = new SmellyClass();
+			classy.NOM(restartReading(file.getParent(), file.getName()));
+			System.out.println("-------------------------");
+			classy.WMC(restartReading(file.getParent(), file.getName()));
+			System.out.println("-------------------------");
+			classy.CYCLO_Method(restartReading(file.getParent(), file.getName()));
+			System.out.println("-------------------------");
+			classy.LOC_Method(restartReading(file.getParent(), file.getName()));
+			System.out.println("-------------------------");
+			classy.LOC_Class(restartReading(file.getParent(), file.getName()));
+			System.out.println("-------------------------");
+			classy.isLongMethod(5, 2, true, true, false);
+			System.out.println("-------------------------");
+			classy.isGodClass(15, 10, 100, true, true, true, true, true);
+			eexport.setClassAndPackageNames(file.getName(), file.getParent());
+			eexport.saveMetrics(classy);
 		}
-	
+		eexport.exportToExcel(eexport.chooseName());
+
+		//Ler um excel, guardar, e exportar um igual
+		eexport.getExcelDataAsMap("-1");
+		eexport.exportToExcel(eexport.chooseName());
+	}
+
 	public static ArrayList<File> RecursiveFinder(File[] arr,int index,int level, ArrayList<File> toreturn) 
-    {
-        if(index == arr.length)
-            return toreturn;
-          
-          
-        if(arr[index].isFile()){
-        	if (arr[index].getName().endsWith(".java")) {
-        		toreturn.add(arr[index]);
-        	}
-        }
-          
-        else if(arr[index].isDirectory())
-        {              
-            RecursiveFinder(arr[index].listFiles(), 0, level + 1, toreturn);
-        }
-           
-        RecursiveFinder(arr,++index, level, toreturn);
-        
-        return toreturn;
-   }
-     
-   public static ArrayList<File> readEverything(){
-	   ArrayList<File> vazio = new ArrayList<File>();
-	   JFileChooser chooser = new JFileChooser(".");
-	   chooser.setMultiSelectionEnabled(true);
-	   chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-	   int ret = chooser.showOpenDialog(null);
-	   if(ret == JFileChooser.APPROVE_OPTION) {
-		   File fileDirectory = chooser.getSelectedFile();
-		   if(fileDirectory.exists() && fileDirectory.isDirectory()){
-			   File arr[] = fileDirectory.listFiles();
-             
-			   RecursiveFinder(arr,0,0,vazio);
-		   }
-		   
-      }
-	  return vazio;
-   }
+	{
+		if(index == arr.length)
+			return toreturn;
+
+
+		if(arr[index].isFile()){
+			if (arr[index].getName().endsWith(".java")) {
+				toreturn.add(arr[index]);
+			}
+		}
+
+		else if(arr[index].isDirectory())
+		{              
+			RecursiveFinder(arr[index].listFiles(), 0, level + 1, toreturn);
+		}
+
+		RecursiveFinder(arr,++index, level, toreturn);
+
+		return toreturn;
+	}
+
+	public static ArrayList<File> readEverything(){
+		ArrayList<File> vazio = new ArrayList<File>();
+		JFileChooser chooser = new JFileChooser(".");
+		chooser.setMultiSelectionEnabled(true);
+		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int ret = chooser.showOpenDialog(null);
+		if(ret == JFileChooser.APPROVE_OPTION) {
+			File fileDirectory = chooser.getSelectedFile();
+			if(fileDirectory.exists() && fileDirectory.isDirectory()){
+				File arr[] = fileDirectory.listFiles();
+
+				RecursiveFinder(arr,0,0,vazio);
+			}
+
+		}
+		return vazio;
+	}
 }
